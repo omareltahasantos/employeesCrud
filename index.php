95fa26d6d01f91981810c42c9e0aa766da7362ce
Omar El Taha Santos <52071546+omareltahasantos@users.noreply.github.com>
2021-09-14 13:49:24 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 // Conecta a la base de datos  con usuario, contraseña y nombre de la BD
-$servidor = "localhost"; $usuario = "root"; $contrasenia = ""; $nombreBaseDatos = "empleados";
+$servidor = "eu-cdbr-west-01.cleardb.com/"; $usuario = "b8ec321a01cc9e"; $contrasenia = "8689f43e"; $nombreBaseDatos = "heroku_f47577e5bf3f176";
 $conexionBD = new mysqli($servidor, $usuario, $contrasenia, $nombreBaseDatos);
 
 
 // Consulta datos y recepciona una clave para consultar dichos datos con dicha clave
 if (isset($_GET["consultar"])){
-    $sqlEmpleaados = mysqli_query($conexionBD,"SELECT * FROM empleados WHERE id=".$_GET["consultar"]);
+    $sqlEmpleaados = mysqli_query($conexionBD,"SELECT * FROM usuarios WHERE id=".$_GET["consultar"]);
     if(mysqli_num_rows($sqlEmpleaados) > 0){
         $empleaados = mysqli_fetch_all($sqlEmpleaados,MYSQLI_ASSOC);
         echo json_encode($empleaados);
@@ -25,7 +25,7 @@ if (isset($_GET["consultar"])){
 
 //borrar pero se le debe de enviar una clave ( para borrado )
 if (isset($_GET["borrar"])){
-    $sqlEmpleaados = mysqli_query($conexionBD,"DELETE FROM empleados WHERE id=".$_GET["borrar"]);
+    $sqlEmpleaados = mysqli_query($conexionBD,"DELETE FROM usuarios WHERE id=".$_GET["borrar"]);
     if($sqlEmpleaados){
         echo json_encode(["success"=>1]);
         exit();
@@ -39,7 +39,7 @@ if(isset($_GET["insertar"])){
     $correo=$data->correo;
         if(($correo!="")&&($nombre!="")){
             
-    $sqlEmpleaados = mysqli_query($conexionBD,"INSERT INTO empleados(nombre,correo) VALUES('$nombre','$correo') ");
+    $sqlEmpleaados = mysqli_query($conexionBD,"INSERT INTO usuarios(nombre,correo) VALUES('$nombre','$correo') ");
     echo json_encode(["success"=>1]);
         }
     exit();
@@ -53,13 +53,13 @@ if(isset($_GET["actualizar"])){
     $nombre=$data->nombre;
     $correo=$data->correo;
     
-    $sqlEmpleaados = mysqli_query($conexionBD,"UPDATE empleados SET nombre='$nombre',correo='$correo' WHERE id='$id'");
+    $sqlEmpleaados = mysqli_query($conexionBD,"UPDATE usuarios SET nombre='$nombre',correo='$correo' WHERE id='$id'");
     echo json_encode(["success"=>1]);
     exit();
 }
   //Devuelve todos los registros en orden descendiente por el id
 if (isset($_GET["ordenarDescId"]) && isset($_GET["limitFirstParam"]) && isset($_GET["limitSecondParam"] ) ) {
-    $sqlEmpleaadosOrderId= mysqli_query($conexionBD,"SELECT DISTINCT * FROM empleados LIMIT ".$_GET["limitFirstParam"].",".$_GET["limitSecondParam"]); //ORDER BY id DESC
+    $sqlEmpleaadosOrderId= mysqli_query($conexionBD,"SELECT DISTINCT * FROM usuarios LIMIT ".$_GET["limitFirstParam"].",".$_GET["limitSecondParam"]); //ORDER BY id DESC
     if(mysqli_num_rows($sqlEmpleaadosOrderId) > 0){
        $empleaadosOrderId = mysqli_fetch_all($sqlEmpleaadosOrderId,MYSQLI_ASSOC);
       
@@ -73,7 +73,7 @@ if (isset($_GET["ordenarDescId"]) && isset($_GET["limitFirstParam"]) && isset($_
 //Devuelve todos los registros en orden ascendente por el id
 
 if (isset($_GET["ordenarAscId"]) && isset($_GET["limitFirstParam"]) && isset($_GET["limitSecondParam"] )) {
-    $sqlEmpleaadosOrderId= mysqli_query($conexionBD,"SELECT DISTINCT * FROM empleados LIMIT ".$_GET["limitFirstParam"].",".$_GET["limitSecondParam"]); //ORDER BY id ASC
+    $sqlEmpleaadosOrderId= mysqli_query($conexionBD,"SELECT DISTINCT * FROM usuarios LIMIT ".$_GET["limitFirstParam"].",".$_GET["limitSecondParam"]); //ORDER BY id ASC
     if(mysqli_num_rows($sqlEmpleaadosOrderId) > 0){
         $empleaadosOrderId = mysqli_fetch_all($sqlEmpleaadosOrderId,MYSQLI_ASSOC);
         echo json_encode($empleaadosOrderId);
@@ -84,7 +84,7 @@ if (isset($_GET["ordenarAscId"]) && isset($_GET["limitFirstParam"]) && isset($_G
 
 //Devuelve todos los registros en orden descendiente por el nombre
 if (isset($_GET["ordenarDescNombre"]) && isset($_GET["limitFirstParam"]) && isset($_GET["limitSecondParam"] ) ) {
-    $sqlEmpleaadosOrderId= mysqli_query($conexionBD,"SELECT DISTINCT * FROM empleados LIMIT ".$_GET["limitFirstParam"].",".$_GET["limitSecondParam"]); //ORDER BY id DESC
+    $sqlEmpleaadosOrderId= mysqli_query($conexionBD,"SELECT DISTINCT * FROM usuarios LIMIT ".$_GET["limitFirstParam"].",".$_GET["limitSecondParam"]); //ORDER BY id DESC
     if(mysqli_num_rows($sqlEmpleaadosOrderId) > 0){
        $empleaadosOrderId = mysqli_fetch_all($sqlEmpleaadosOrderId,MYSQLI_ASSOC);
       
@@ -98,7 +98,7 @@ if (isset($_GET["ordenarDescNombre"]) && isset($_GET["limitFirstParam"]) && isse
 //Devuelve todos los registros en orden ascendente por el nombre
 
 if (isset($_GET["ordenarAscNombre"]) && isset($_GET["limitFirstParam"]) && isset($_GET["limitSecondParam"] )) {
-    $sqlEmpleaadosOrderId= mysqli_query($conexionBD,"SELECT DISTINCT * FROM empleados LIMIT ".$_GET["limitFirstParam"].",".$_GET["limitSecondParam"]); //ORDER BY id ASC
+    $sqlEmpleaadosOrderId= mysqli_query($conexionBD,"SELECT DISTINCT * FROM usuarios LIMIT ".$_GET["limitFirstParam"].",".$_GET["limitSecondParam"]); //ORDER BY id ASC
     if(mysqli_num_rows($sqlEmpleaadosOrderId) > 0){
         $empleaadosOrderId = mysqli_fetch_all($sqlEmpleaadosOrderId,MYSQLI_ASSOC);
         echo json_encode($empleaadosOrderId);
@@ -109,7 +109,7 @@ if (isset($_GET["ordenarAscNombre"]) && isset($_GET["limitFirstParam"]) && isset
 
 //Devuelve todos los registros en orden descendiente por el correo
 if (isset($_GET["ordenarDescCorreo"]) && isset($_GET["limitFirstParam"]) && isset($_GET["limitSecondParam"] ) ) {
-    $sqlEmpleaadosOrderId= mysqli_query($conexionBD,"SELECT DISTINCT * FROM empleados LIMIT ".$_GET["limitFirstParam"].",".$_GET["limitSecondParam"]); //ORDER BY id DESC
+    $sqlEmpleaadosOrderId= mysqli_query($conexionBD,"SELECT DISTINCT * FROM usuarios LIMIT ".$_GET["limitFirstParam"].",".$_GET["limitSecondParam"]); //ORDER BY id DESC
     if(mysqli_num_rows($sqlEmpleaadosOrderId) > 0){
        $empleaadosOrderId = mysqli_fetch_all($sqlEmpleaadosOrderId,MYSQLI_ASSOC);
       
@@ -123,7 +123,7 @@ if (isset($_GET["ordenarDescCorreo"]) && isset($_GET["limitFirstParam"]) && isse
 //Devuelve todos los registros en orden ascendente por el correo
 
 if (isset($_GET["ordenarAscCorreo"]) && isset($_GET["limitFirstParam"]) && isset($_GET["limitSecondParam"] )) {
-    $sqlEmpleaadosOrderId= mysqli_query($conexionBD,"SELECT DISTINCT * FROM empleados LIMIT ".$_GET["limitFirstParam"].",".$_GET["limitSecondParam"]); //ORDER BY id ASC
+    $sqlEmpleaadosOrderId= mysqli_query($conexionBD,"SELECT DISTINCT * FROM usuarios LIMIT ".$_GET["limitFirstParam"].",".$_GET["limitSecondParam"]); //ORDER BY id ASC
     if(mysqli_num_rows($sqlEmpleaadosOrderId) > 0){
         $empleaadosOrderId = mysqli_fetch_all($sqlEmpleaadosOrderId,MYSQLI_ASSOC);
         echo json_encode($empleaadosOrderId);
@@ -133,7 +133,7 @@ if (isset($_GET["ordenarAscCorreo"]) && isset($_GET["limitFirstParam"]) && isset
 }
 //Devuelve una cantidad de registros dependiendo del Limit
 if ((isset($_GET["limitFirstParam"]) && isset($_GET["limitSecondParam"] )) && !isset($_GET["ordenarAscId"]) && !isset($_GET["ordenarDescId"]) && !isset($_GET["ordenarAscNombre"]) && !isset($_GET["ordenarDescNombre"]) && !isset($_GET["ordenarAscCorreo"]) && !isset($_GET["ordenarDescCorreo"]) ) {
-    $sqlEmpleaadosByLimit= mysqli_query($conexionBD,"SELECT * FROM empleados LIMIT ".$_GET["limitFirstParam"].",".$_GET["limitSecondParam"]);
+    $sqlEmpleaadosByLimit= mysqli_query($conexionBD,"SELECT * FROM usuarios LIMIT ".$_GET["limitFirstParam"].",".$_GET["limitSecondParam"]);
     if(mysqli_num_rows($sqlEmpleaadosByLimit) > 0){
         $empleaadosByLimit = mysqli_fetch_all($sqlEmpleaadosByLimit,MYSQLI_ASSOC);
        echo json_encode($empleaadosByLimit);
@@ -143,7 +143,7 @@ if ((isset($_GET["limitFirstParam"]) && isset($_GET["limitSecondParam"] )) && !i
 }
 //Devuelve cantidad de registros que tiene una tabla
 if (isset($_GET["count"])) {
-    $sqlEmpleaados = mysqli_query($conexionBD,"SELECT * FROM empleados ");
+    $sqlEmpleaados = mysqli_query($conexionBD,"SELECT * FROM usuarios ");
     $numero_filas = mysqli_num_rows($sqlEmpleaados);
     echo $numero_filas;
 
